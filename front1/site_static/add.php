931972526dfb3108bef56f0575268f8ac1646d7d
Grigorii Kozlov <49227124+fogmorn@users.noreply.github.com>
2021-11-14 11:53:15 +0300

    Add: Docker for front

---
+++ front1/site_static/add.php
@@ -0,0 +1,11 @@
+<?php
+if (isset($_GET['first']) && isset($_GET['second'])) {
+ 
+    $num1 = $_GET['first'];
+    $num2 = $_GET['second'];
+    $result = $num1 + $num2;
+    
+    echo $result . "<br />";
+    echo 'Hostname: \'' . gethostname() . '\'';
+}
+?>
